9d1e63daaec3649ccdad12779d14847357c13734
19miguel19lim <tech@paragonmedia.io>
2025-12-19 06:14:29 +0800

    Fix logo path for production: Move logo.svg to public folder and update Navbar reference

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div className="w-full bg-gray-200">
       <div className="max-w-6xl mx-auto  flex items-center justify-between px-4 py-2 ">
-        <img src="/src/assets/logo.svg" className="h-24" alt="" />
+        <img src="/logo.svg" className="h-24" alt="ParagonMedia Logo" />
         <NavbarMenu />
       </div>
     </div>
